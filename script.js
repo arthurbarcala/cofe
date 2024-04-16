e478069aa21066d26b9518c5787342ee1597aeb2
Arthur Barcala <arthurfbarcalars@gmail.com>
2024-04-16 07:11:32 -0300

    correção de css + easter egg hehe

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ socket.on("currentName", person => {
 document.getElementsByTagName("button")[0].addEventListener("click", () => {
     window.alert("Essa feature ainda não está disponível! 😭")
 })
+
+document.getElementById("emojo").addEventListener("click", () => {
+    const emojis = [
+        "😎", "🤓", "🥳", "🤩", "😻", "💪", "👍", "✌️", "🙌", "👏",
+        "🙏", "🤞", "🤟", "🤘", "👊", "🤙", "👌", "🤝", "👋", "🖖",
+        "🤗", "🥺", "😬", "😷", "🤒", "🤕", "😴", "🥱", "😵", "🤯",
+        "🐶", "🐱", "🐭", "🐹", "🐰", "🐻", "🐼", "🦊", "🐮", "🐷",
+        "🐸", "🐔", "🐧", "🐦", "🐤", "🐣", "🐥", "🦆", "🦅", "🦉",
+        "🍕", "🍔", "🍟", "🌮", "🍦", "🍰", "🍩", "🍭", "🍹", "🥤"
+      ];
+
+    const randIndex = Math.round(Math.random()*(emojis.length-1))
+    
+    document.getElementById("emojo").innerHTML = emojis[randIndex]
+})
